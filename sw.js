9c555058ce732a324c946322317550c455d5a1ef
niloc515 <cfhd@icloud.com>
2019-05-20 22:57:17 -0500

    Restore PWA functionality

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
       caches.open('gpaCalculator').then(function(cache) {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/css/gpawithstyle.css',
-          '/js/gpa.js',
+          './',
+          './index.html',
+          './css/gpawithstyle.css',
+          './js/gpa.js',
           'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-          '/images/favicon.ico'
+          './images/favicon.ico'
         ]);
       })
     );
